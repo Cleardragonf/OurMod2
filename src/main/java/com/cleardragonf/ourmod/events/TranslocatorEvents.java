3e50acd99957b6d93604270b06d222e46c50b2d0
chrism339@hotmail.com <chrism339@hotmail.com>
2022-03-30 14:51:56 -0500

    •  BUGFIX:  Fixed Right click resetting all items rather then Translocator Tuner.

--- src/main/java/com/cleardragonf/ourmod/events/TranslocatorEvents.java
+++ src/main/java/com/cleardragonf/ourmod/events/TranslocatorEvents.java
@@ -1,6 +1,7 @@
 package com.cleardragonf.ourmod.events;
 
 import com.cleardragonf.ourmod.items.TranslocatorTuner;
+import com.cleardragonf.ourmod.setup.Registration;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -9,9 +10,12 @@ public class TranslocatorEvents {
     @SubscribeEvent
     public static void onUse(PlayerInteractEvent.RightClickItem event){
         if(!event.getWorld().isClientSide()){
-            TranslatableComponent text = new TranslatableComponent("Resetting");
-            event.getPlayer().sendMessage(text, event.getPlayer().getUUID());
-            event.getItemStack().removeTagKey("energypos");
+            if(event.getItemStack().is(Registration.TRANSLOCATOR_TUNER.get())){
+                TranslatableComponent text = new TranslatableComponent("Resetting");
+                event.getPlayer().sendMessage(text, event.getPlayer().getUUID());
+                event.getItemStack().removeTagKey("energypos");
+            }
+
         }
 
     }
